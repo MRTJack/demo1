085d1a1af248a4a0e6f0fc3afcda04bb59d47b93
MRTJack <alizanovmarat1@gmail.com>
2024-07-16 19:04:49 +0500

    Revert "demo1 uchun"
    This reverts commit baac4a093792d22f1fe082b3981170743f30ffd2.

--- src/main/java/uz/pdp/app_hr/models/enums/RoleName.java
+++
@@ -1,7 +0,0 @@
-package uz.pdp.app_hr.models.enums;
-public enum RoleName {
-    ROLE_DIRECTOR,
-    ROLE_HR_MANAGER,
-    ROLE_MANAGER,
-    ROLE_EMPLOYEE
-}
--- src/main/java/uz/pdp/app_hr/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package uz.pdp.app_hr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.pdp.app_hr.models.Role;
-import uz.pdp.app_hr.models.enums.RoleName;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByRoleName(RoleName roleName);
-}
